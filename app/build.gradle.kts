ffcaefca95164b2f4fd82071529dec04155ea041
Vishal Telangre <vishaltelangre@gmail.com>
2026-03-02 12:27:36 +0530

    Bump version and add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vishaltelangre.nerdcalci"
         minSdk = 23
         targetSdk = 35
-        versionCode = 150
-        versionName = "1.5.0"
+        versionCode = 151
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

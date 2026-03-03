18666125c41ad1fa36dd2c67fbdd74e8845fa272
Vishal Telangre <vishaltelangre@gmail.com>
2026-03-03 23:09:04 +0530

    [relase] Bump version to v1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vishaltelangre.nerdcalci"
         minSdk = 23
         targetSdk = 35
-        versionCode = 151
-        versionName = "1.5.1"
+        versionCode = 160
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

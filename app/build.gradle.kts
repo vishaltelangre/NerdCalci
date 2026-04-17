64f14eae574dc46ff806a29158fa109cff8e1682
Vishal Telangre <vishaltelangre@gmail.com>
2026-04-18 00:36:20 +0530

    Enable R8 minification, resource shrinking, and remove unused library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -76,7 +77,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material:material-icons-extended")
-    implementation("com.google.android.material:material:1.13.0")
 
     // Room (Local database)
     implementation("androidx.room:room-runtime:2.7.0")
